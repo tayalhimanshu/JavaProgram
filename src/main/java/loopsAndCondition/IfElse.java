cd4b70719cae9bc5e68f5446fe56899ac4b6ad53
tayalhimanshu <tayal.1991@gmail.com>
2024-10-05 20:01:38 +0530

    more programs

--- src/main/java/loopsAndCondition/IfElse.java
+++ src/main/java/loopsAndCondition/IfElse.java
@@ -3,7 +3,8 @@ package loopsAndCondition;
 public class IfElse {
 	public static void main(String[] args) {
 		ifExample();
-		
+		ternaryOperator(12);
+		ternaryOperator(13);
 	}
 	
 	public static void ifExample() {
@@ -12,6 +13,11 @@ public class IfElse {
 			System.out.println("Inside If");
 		else
 			System.out.println("Inside Else");
+	}
+	
+	public static void ternaryOperator(int number) {
+		String output = (number%2==0)?"Even":"Odd";
+		System.out.println(number+" is an "+output+" number");
 	}
 }
 
---
+++ src/main/java/loopsAndCondition/switchStatement.java
@@ -0,0 +1,31 @@
+package loopsAndCondition;
+
+public class switchStatement {
+	public static void main(String[] args) {
+		printDay(5);
+		printDay(10);
+	}
+	
+	public static void printDay(int number) {
+		switch(number) {
+		case 1 : System.out.println("Monday");
+		break;
+		case 2 : System.out.println("Tuesday");
+		break;
+		case 3 : System.out.println("Wednesday");
+		break;
+		case 4 : System.out.println("Thursday");
+		break;
+		case 5 : System.out.println("Friday");
+		break;
+		case 6 : System.out.println("Saturday");
+		break;
+		case 7 : System.out.println("Sunday");
+		break;
+		default: System.out.println("This is incorrect number");
+		break;
+		}
+		
+	}
+}
+
