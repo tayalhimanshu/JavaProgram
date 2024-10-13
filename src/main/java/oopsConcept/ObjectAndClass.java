4dc16728fb103dd0dd35701458de9b426b92b0cd
tayalhimanshu <tayal.1991@gmail.com>
2024-10-13 16:57:00 +0530

    OOPS Concept

---
+++ src/main/java/oopsConcept/ObjectAndClass.java
@@ -0,0 +1,28 @@
+package oopsConcept;
+
+//	A Class is a blueprint from which an object is created
+
+/*	A class contains:
+		Fields
+		Methods
+		Constructors
+		Block
+		Nested Class and Interface
+*/
+
+public class ObjectAndClass {
+	
+/*	LOCAL Variables :- Defined inside methods, constructors or blocks.
+					   Declared & initialized within the method & 
+					   the variable will be destroyed when the method has completed.
+*/	
+	
+/*	INSTANCE Variable :- A variable which is created inside the class but outside method.
+	 					Instance variable doesn't get memory at compile time. 
+						It gets memory at runtime when an object or instance is created.
+*/
+	
+//	CLASS Variable :- Declared within class, outside any method with Static keyword.
+	
+	
+}
